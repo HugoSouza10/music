16320037b386fb3e9967a0f51cf24c3247030ce9
Hugo Souza <hugo.html.com@gmail.com>
2021-06-23 09:13:22 -0300

    Modificando menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 let buttonMobile = document.getElementById('icone_mobile');
-let menu = document.getElementById('#menu');
+let menuMain = document.querySelector(' #menu ');
+
 
 buttonMobile.addEventListener('click', ()=>{
     
     if(buttonMobile.classList.contains('closeMenu')){
        buttonMobile.classList.remove('closeMenu');
-       buttonMobile.classList.add('menuOpen');
+       menuMain.style.marginRight='-400px';
+      
+       
     }else{
        buttonMobile.classList.add('closeMenu');
+       menuMain.style.marginRight='0px';
+      
+      
     }
 });
