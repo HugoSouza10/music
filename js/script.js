c1e78a1ed9a42b2901329a3fa2ec39661eb4f1e2
Hugo Souza <hugo.html.com@gmail.com>
2021-06-23 10:48:51 -0300

    Modificando efeito do menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let buttonMobile = document.getElementById('icone_mobile');
 let menuMain = document.querySelector(' #menu ');
+let modal_Box = document.querySelector('.modal_box');
 
 
 buttonMobile.addEventListener('click', ()=>{
@@ -7,11 +8,26 @@ buttonMobile.addEventListener('click', ()=>{
     if(buttonMobile.classList.contains('closeMenu')){
        buttonMobile.classList.remove('closeMenu');
        menuMain.style.marginRight='-400px';
+
+       setTimeout(()=>{
+          modal_Box.classList.add('modalBoxEfectClose');
+          modal_Box.style.display='none';
+       },100);
+     
       
        
     }else{
        buttonMobile.classList.add('closeMenu');
        menuMain.style.marginRight='0px';
+
+
+       setTimeout(() => {
+          modal_Box.classList.remove('modalBoxEfectClose');
+          modal_Box.style.display='block';
+
+       }, 100);
+       
+       
       
       
     }
